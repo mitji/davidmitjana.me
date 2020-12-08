eba826bdfcebd7092d0a39602136d47ca490743c
mitji <d.mitjana@gmail.com>
2020-12-09 00:05:59 +0100

    Added plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,64 @@
 module.exports = {
   plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'assets',
+        path: `${__dirname}/src/assets`
+      }
+    },
     'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: [
+          'Lora:400,500,700',
+          'Source Sans Pro:200,300,400,900',
+          'Open Sans:300,400,800',
+          'Roboto: 100,300,400,500,900,100i,300i,400i,500i,900i',
+          'Montserrat:900'
+        ],
+        display: 'swap'
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        extensions: ['.md', '.mdx'],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1200
+            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/
+        }
+      }
+    }
   ],
 };
