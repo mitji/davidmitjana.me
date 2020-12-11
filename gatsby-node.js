ab912f533495acc417cd041c6715f15972e42d11
mitji <d.mitjana@gmail.com>
2020-12-11 16:15:30 +0100

    Fix posts path

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,7 +21,7 @@ exports.createPages = async({ graphql, actions}) => {
   // create page for each blogpost
   result.data.allMdx.edges.forEach((edge) => {
     createPage({
-      path: `/${edge.node.frontmatter.slug}`,
+      path: `blog/${edge.node.frontmatter.slug}`,
       component: path.resolve('./src/templates/blog-post.tsx'),
       context: {id: edge.node.id}
     })
