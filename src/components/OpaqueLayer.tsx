498f8188ba2b91281cb1702683535317a2bcd748
mitji <d.mitjana@gmail.com>
2021-02-15 00:09:03 +0100

    Creacion OpaqueLayer

---
+++ src/components/OpaqueLayer.tsx
@@ -0,0 +1,106 @@
+import React, { useContext, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { AppContext } from '../utils';
+
+export const ModalWrapper = styled.div`
+  width: 100vw;
+  height: 100%;
+`;
+
+const Opaque = styled.div<{ isOpen: boolean | null }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #000000;
+  z-index: 11;
+  transition: opacity 0.3s;
+  opacity: ${props => props.isOpen ? '0.4' : '0'};
+`
+
+const Modal = styled.div`
+  position: fixed;
+  z-index: 12;
+  top: 0;
+  right: 0;
+  width: 100%;
+  max-width: 50rem;
+  height: 100%;
+  background: var(--color-backgroundBody);
+  padding: 3rem;
+  overflow: auto;
+  transform: translate3d(100%,0,0);
+  transition: transform 0.3s 0.1s;
+  /* width */
+  &::-webkit-scrollbar {
+      display: none;
+    }
+    /* Track */
+    &::-webkit-scrollbar-track {
+      border: 1px solid #64a1f6;
+    }
+    
+    /* Handle */
+    &::-webkit-scrollbar-thumb {
+      background: #7f91b936;
+    }
+    
+    /* Handle on hover */
+    &::-webkit-scrollbar-thumb:hover {
+      background: #86f664;
+    }
+  &.modal-open {
+    transform: translate3d(0%,0,0);
+  }
+  &.modal-close {
+    transform: translate3d(100%,0,0);
+  }
+  .close-btn {
+    transform: translate3d(-80%, -80%, 0);
+    background: none;
+    border: none;
+    color: var(--color-text);
+    font-size: 2rem;
+    cursor: pointer;
+    outline: none;
+  }
+`
+
+type OpaqueLayerProps = {
+  component: React.ReactNode;
+}
+
+export function OpaqueLayer( props: OpaqueLayerProps ) {
+
+  const { component } = props;
+  const { isSettingsOpen, setIsSettingsOpen} = useContext(AppContext);
+  const [open, setOpen] = useState<boolean | null>(null);
+
+  function closeLayer() {
+    setOpen(false);
+    setTimeout(() => setIsSettingsOpen(false), 300);
+  }
+
+  useEffect(() => {
+    if (isSettingsOpen) {
+      setOpen(true)
+    } else if (isSettingsOpen === false) {
+      setOpen(false);
+    }
+  }, [isSettingsOpen]);
+
+  return (
+    isSettingsOpen ? (
+      <ModalWrapper>
+        <Opaque isOpen={open} onClick={closeLayer} />
+        <Modal className={open ? 'modal-open' : 'modal-close'}>
+          <button type="button" className="close-btn" onClick={closeLayer}>
+            &#10005;
+          </button>
+          {component}
+        </Modal> 
+      </ModalWrapper>
+    ) : null
+  )
+}
